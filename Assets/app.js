482a7f530f5b326936b3bde5e808099b3ba2559f
Mahamadou Kabba <mkabba34@gmail.com>
2022-03-23 13:11:03 -0400

    added main to html

--- Assets/app.js
+++ Assets/app.js
@@ -1,15 +1,17 @@
 // Create variables to store the quiz questions
 
-// Use mouse-click events to start the quiz
+// Use mouse-click events to start the quiz (start button)
 
 // Write for loops to cycle through quiz questions
 
 // Use key-press events to receive user input in the form of answers to quiz questions
 
-// Create a time limit for the game using time functions
+// Create a time limit for the game using time functions (set timer)
 
 // Write conditional statements to determine wrong and right answers
 
-// Use client-side storage to store high scores
+// Use client-side storage to store high scores (I can save my initials and score)
+
+// display questions
 
 // // Use GitHub Pages to publish the page to the we
